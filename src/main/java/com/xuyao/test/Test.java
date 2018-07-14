d05a94d94056858ab2b4b9dea5f2d11722316e01
xuyao <xuyao0@foxmail.com>
2018-07-14 15:58:06 +0800

    整理

--- src/main/java/com/xuyao/test/Test.java
+++ src/main/java/com/xuyao/test/Test.java
@@ -1,22 +1,8 @@
 package com.xuyao.test;
 
-import com.xuyao.utils.NetworkUtils;
-
 public class Test {
+    public static void main(String[] args) throws InterruptedException {
 
-	public static void main(String[] args) {
-		String host = "127.0.0.1";
-		int port = 9092;
-		int timeout = 5000;
-		long b = System.currentTimeMillis();
-		System.out.println(NetworkUtils.isHostConnectable(host, port, timeout));
-		long a = System.currentTimeMillis();
-		System.out.println(a-b);
-		
-		b = System.currentTimeMillis();
-		System.out.println(NetworkUtils.isHostReachable(host, timeout));
-		a = System.currentTimeMillis();
-		System.out.println(a-b);
-	}
+    }
 
 }
